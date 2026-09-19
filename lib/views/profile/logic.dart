5af5258d11fcd7e375244301c893fad5f1bd8dfc
Bilal Saeed <mianzainjh@gmail.com>
2026-09-19 08:56:22 +0500

    Fixed the error facing while running the app..

--- lib/views/profile/logic.dart
+++ lib/views/profile/logic.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:documind/views/logIn/view.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
